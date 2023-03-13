2e3b1cbac6d091e600ed8c8bd5f3897a3c4b812e
bigman <terrence.chebii@student.moringaschool.com>
2023-03-14 01:08:35 +0300

    wuehh ngori

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.ceil(Math.random()) + 1;
+
+num3 = 24
+let mod = num3 / 5
+console.log(mod)
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,20)
+console.log(max)
